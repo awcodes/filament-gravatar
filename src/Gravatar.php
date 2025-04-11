503b9d41329cbfb1006fde0a79aba3823b74f461
Adam Weston <adam@awcodes.com>
2025-04-11 16:08:02 -0400

    Feat: support initials and color in default

--- src/Gravatar.php
+++ src/Gravatar.php
@@ -7,12 +7,12 @@ class Gravatar
     /**
      * Get either a Gravatar URL or complete image tag for a specified email address.
      *
-     * @param  string|null  $email The email address
-     * @param  int  $size Size in pixels, defaults to 80px [ 1 - 2048 ]
-     * @param  string  $default Default imageset to use [ 404 | mp | identicon | monsterid | wavatar | robohash ]
-     * @param  string  $rating Maximum rating (inclusive) [ g | pg | r | x ]
-     * @param  bool  $asImage True to return a complete IMG tag False for just the URL
-     * @param  array  $attributes Optional, additional key/value attributes to include in the IMG tag
+     * @param  string|null  $email  The email address
+     * @param  int  $size  Size in pixels, defaults to 80px [ 1 - 2048 ]
+     * @param  string  $default  Default imageset to use [ initials | color | 404 | mp | identicon | monsterid | wavatar | robohash ]
+     * @param  string  $rating  Maximum rating (inclusive) [ g | pg | r | x ]
+     * @param  bool  $asImage  True to return a complete IMG tag False for just the URL
+     * @param  array  $attributes  Optional, additional key/value attributes to include in the IMG tag
      * @return string containing either just a URL or a complete image tag
      *
      * @source https://gravatar.com/site/implement/images/php/
@@ -32,13 +32,13 @@ class Gravatar
 
         $url = 'https://www.gravatar.com/avatar';
         if ($email) {
-            $url .= '/' . md5(strtolower(trim($email)));
+            $url .= '/'.md5(mb_strtolower(trim($email)));
         }
         $url .= "?s=$size&d=$default&r=$rating";
         if ($asImage) {
-            $url = '<img src="' . $url . '"';
+            $url = '<img src="'.$url.'"';
             foreach ($attributes as $key => $val) {
-                $url .= ' ' . $key . '="' . $val . '"';
+                $url .= ' '.$key.'="'.$val.'"';
             }
             $url .= ' />';
         }
